83d1ac997030488eecb4cbdf5ab464a6c051a130
a02456 <li.yang@casstime.com>
2023-01-31 20:07:06 +0800

    feat: 打包完善 + demo 完善

--- vite.config.ts
+++ vite.config.ts
@@ -1,52 +1,54 @@
 /// <reference types="vitest" />
 
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import vueJsx from '@vitejs/plugin-vue-jsx';
-import legacy from '@vitejs/plugin-legacy';
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import legacy from '@vitejs/plugin-legacy'
 
-import unocss from './config/unocss';
+import unocss from './config/unocss'
 
 const rollupOptions = {
-  external: ['vue', 'vue-router'],
-  output: {
-    globals: {
-      vue: 'Vue',
+    external: ['vue', 'vue-router'],
+    output: {
+        globals: {
+            vue: 'Vue',
+        },
     },
-  },
-};
+}
 
 export default defineConfig({
-  resolve: {
-    alias: [
-      {
-        find: 'vue',
-        replacement: 'vue/dist/vue.esm-bundler',
-      },
-    ],
-  },
-  plugins: [vue(), vueJsx({}), unocss()],
-  build: {
-    rollupOptions,
-    cssCodeSplit: true, // 追加
-    minify: false,
-    lib: {
-      entry: './src/entry.ts',
-      formats: ['umd', 'es'],
-      name: 'HUI',
-      fileName: 'h-ui',
-      // 导出模块格式
+    resolve: {
+        alias: [
+            {
+                find: 'vue',
+                replacement: 'vue/dist/vue.esm-bundler',
+            },
+        ],
     },
-  },
-  test: {
-    // enable jest-like global test APIs
-    globals: true,
-    // simulate DOM with happy-dom
-    // (requires installing happy-dom as a peer dependency)
-    environment: 'happy-dom',
-    // 支持tsx组件，很关键
-    transformMode: {
-      web: [/.[tj]sx$/],
+    plugins: [vue(), vueJsx({}), unocss()],
+    build: {
+        rollupOptions,
+        cssCodeSplit: true, // 样式打包成单独的文件
+        minify: false, // boolean | 'terser' | 'esbuild'
+        sourcemap: true, // 输出单独 source文件
+        lib: {
+            entry: './src/entry.ts', // 入口
+            formats: ['umd', 'es', 'cjs'], // 构建产物格式
+            fileName: (fmt, entryName) => {
+                return `h-ui.${fmt}.js`
+            }, // 导出的模块格式
+            name: 'HUI', // UMD IIFE 全局变量的名字
+        },
     },
-  },
-});
+    test: {
+        // enable jest-like global test APIs
+        globals: true,
+        // simulate DOM with happy-dom
+        // (requires installing happy-dom as a peer dependency)
+        environment: 'happy-dom',
+        // 支持tsx组件，很关键
+        transformMode: {
+            web: [/.[tj]sx$/],
+        },
+    },
+})
